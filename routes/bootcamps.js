b324bee380dcaea8dbeaa8be80d657cf7c2f8c3f
AleksanderWojtkowski <aleksanderwojtkowski@gmail.com>
2020-12-23 19:47:24 +0100

    work in progress

---
+++ routes/bootcamps.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius,
+} = require("../controllers/bootcamps");
+
+//Include other resource router
+const courseRouter = require("./courses");
+
+const router = express.Router();
+
+//Reroute into other resource routers
+router.use("/:bootcampId/courses", courseRouter);
+
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
+
+router.route("/").get(getBootcamps).post(createBootcamp);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
+const morgan = require("morgan");
+const colors = require("colors");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+//Connect to DB
+connectDB();
+
+const app = express();
+
+//Body parser
+app.use(express.json());
+
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.rainbow
+  )
+);
+//Handle unhandled rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //close server and exit process'
+  server.close(() => process.exit(1));
+});
